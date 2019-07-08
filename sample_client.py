726bb8aeab10fbf4e09837423afacd88eec2c2f7
Edison Abahurire <abahedison1@outlook.com>
2019-07-08 11:57:22 +0300

    republish package

--- sample_client.py
+++ sample_client.py
@@ -1,10 +1,13 @@
+'''This could be your example consumer file from which you will receive and process the API responses'''
 import bmrs
 
 
 def on_message(json_response):
+    '''Receive a JSON reponse and do stuff with it'''
     print(list(json_response.keys()))
     # print(json_response)
 
+# connect to the bmrs API using your credentials
 bmrs.connect_to_api(
         api_key = 'kslch6aa8p97wyn',
         client_id = 89898989,
