ce89f8a768b34bc3f702fb8e60fac2897643ca9e
Rwothoromo Elijah <rwothoromoelijah@gmail.com>
2019-08-02 18:46:57 +0300

    Fixed multiple spellings and added instructions on the README on how to get the API key

--- bmrs.py
+++ bmrs.py
@@ -10,6 +10,7 @@ import json
 
 class MyListener(stomp.ConnectionListener):
     '''This is a listener class that listens for new messages using the STOMP protocol'''
+
     def __init__(self, listener):
         self.listener = listener
 
@@ -18,19 +19,19 @@ class MyListener(stomp.ConnectionListener):
 
     def on_message(self, headers, message):
         message = xmltodict.parse(message)
-        for key,value in headers.items():
-            message[key]=value
+        for key, value in headers.items():
+            message[key] = value
             # print('header: key %s , value %s' %(key, value))
         message = json.loads(json.dumps(message))
         self.listener(message)
 
 
 def connect_to_api(api_key='', client_id='', listener='', port=61613):
-    ''' Connect to the BMRS API. This function will get the XML data and convert it to JSON instantaneoudly!
+    ''' Connect to the BMRS API. This function will get the XML data and convert it to JSON instantaneously!
 
     `api_key`: is your API key that can be got by following [Guide](https://www.elexon.co.uk/documents/training-guidance/bsc-guidance-notes/bmrs-api-and-data-push-user-guide-2/).
     `client_id`: is your Client ID that can be got together with the APIkey from the above step.
-    `listener`: is your custom function that recieves & handles messages returned from the API. See a sample of this in our sample file sample_client.py on github.
+    `listener`: is your custom function that receives & handles messages returned from the API. See a sample of this in our sample file sample_client.py on github.
     '''
     # connect to API using stomp
     conn = stomp.Connection12(
@@ -45,6 +46,6 @@ def connect_to_api(api_key='', client_id='', listener='', port=61613):
     # add a listener instance and initialize it with the user's supplied custom listener
     conn.set_listener('', MyListener(listener))
 
-    # check for new meessages after every x seconds
+    # check for new messages after every x seconds
     while conn.is_connected():
         sleep(1)
