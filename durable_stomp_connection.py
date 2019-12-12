b5b21ac2c3c358d445c3e5e6cb23e2a2d862e8b9
Edison Abahurire <edison.abahurire@cobblestoneenergy.com>
2019-12-12 07:30:48 +0000

    format to black

--- durable_stomp_connection.py
+++ durable_stomp_connection.py
@@ -20,10 +20,19 @@ class DurableStompConnection:
     Handles the creation of the durable STOMP connection and the processing of the data
     """
 
-    def __init__(self, api_key,
-                 bmrs_message_processor, logger=CustomLogger(),  client_id=get_hostname(), message_types=[],
-                 bmrs_host='api.bmreports.com', port=61613, durable=True,
-                 poll_interval=1, xml_to_json=True):
+    def __init__(
+        self,
+        api_key,
+        bmrs_message_processor,
+        logger=CustomLogger(),
+        client_id=get_hostname(),
+        message_types=[],
+        bmrs_host="api.bmreports.com",
+        port=61613,
+        durable=True,
+        poll_interval=1,
+        xml_to_json=True,
+    ):
         self.__logger = logger
         self.__durable = durable
         self.__poll_interval = poll_interval
@@ -32,8 +41,9 @@ class DurableStompConnection:
         self.__client_id = client_id
         self.__message_types = message_types
         self.__xml_to_json = xml_to_json
-        self.__connection = stomp.Connection12(host_and_ports=[
-            (bmrs_host, port)], use_ssl=True)
+        self.__connection = stomp.Connection12(
+            host_and_ports=[(bmrs_host, port)], use_ssl=True
+        )
 
     def __diconnection_handler(self, message):
         self.__logger.error(f'ERROR! : "{message}"')
@@ -44,9 +54,10 @@ class DurableStompConnection:
         Generates a selector string that allows all the message_types
         passed to be listened to
         """
-        selector_substrings = ["JMSType='{}'".format(
-            message_type) for message_type in self.__message_types]
-        return ' OR '.join(selector_substrings)
+        selector_substrings = [
+            "JMSType='{}'".format(message_type) for message_type in self.__message_types
+        ]
+        return " OR ".join(selector_substrings)
 
     def __acknowledge_message(self, ack):
         """
@@ -60,19 +71,19 @@ class DurableStompConnection:
         Processes parsed bmrs messages
         """
         if error is not None:
-            self.__logger.error('Error thrown {}'.format(error))
+            self.__logger.error("Error thrown {}".format(error))
 
         try:
-            ack = headers['ack']
+            ack = headers["ack"]
             self.__acknowledge_message(ack)
 
         except KeyError as exception:
-            self.__logger.error('No Message Id received')
+            self.__logger.error("No Message Id received")
             raise exception
 
         if message_dict is not None:
             self.__bmrs_message_processor(message_dict)
-            self.__logger.info('NEW MESSAGE RECEIVED \n\n')
+            self.__logger.info("NEW MESSAGE RECEIVED \n\n")
 
     def __restart(self):
         """
@@ -97,8 +108,12 @@ class DurableStompConnection:
         For durable connections:
             - parameter 'ack' = 'client-individual' for the connection
         """
-        self.__connection.connect(self.__api_key, self.__api_key, wait=True,
-                                  headers={'client-id': self.__client_id})
+        self.__connection.connect(
+            self.__api_key,
+            self.__api_key,
+            wait=True,
+            headers={"client-id": self.__client_id},
+        )
 
     def __subscribe(self):
         """
@@ -114,17 +129,17 @@ class DurableStompConnection:
             for the connection in ActiveMQ versions after 5.6.0
             (note: 'subscriptionName' not 'subcriptionName')
         """
-        acknowledgement_mode = 'client-individual' if self.__durable else 'auto'
+        acknowledgement_mode = "client-individual" if self.__durable else "auto"
         headers = {
-            'activemq.subcriptionName': self.__client_id,
-            'activemq.subscriptionName': self.__client_id,
+            "activemq.subcriptionName": self.__client_id,
+            "activemq.subscriptionName": self.__client_id,
         }
 
         if len(self.__message_types) > 0:
-            headers['selector'] = self.__generate_jms_type_selector_string()
+            headers["selector"] = self.__generate_jms_type_selector_string()
 
         self.__connection.subscribe(
-            destination='/topic/bmrsTopic',
+            destination="/topic/bmrsTopic",
             ack=acknowledgement_mode,
             id=self.__client_id,
             headers=headers,
@@ -135,10 +150,13 @@ class DurableStompConnection:
         Initializes the STOMP client and connects to BMRS and listens
         """
         self.__connection.set_listener(
-            '', StompListener(
+            "",
+            StompListener(
                 self.__process_bmrs_message,
                 disconnection_handler=self.__diconnection_handler,
-                xml_to_json=self.__xml_to_json))
+                xml_to_json=self.__xml_to_json,
+            ),
+        )
         self.__connection.start()
         self.__connect()
         self.__subscribe()
--- test_bmrs.py
+++ test_bmrs.py
@@ -2,10 +2,9 @@ import unittest
 
 
 class TestBmrsReceiver(unittest.TestCase):
-
     def test_connect_to_api(self):
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
