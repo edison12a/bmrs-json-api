730e3429408130fac7e4e01fed9b9e99197b2d41
Edison Abahurire <edison.abahurire@cobblestoneenergy.com>
2020-01-04 14:44:21 +0000

    fix exception handling and add 30 second wait before retry

--- bmrs.py
+++ bmrs.py
@@ -1,6 +1,6 @@
 """This package enables you receive BMRS data as json instead of the default XML"""
-__version__ = "1.2.75"
-
+__version__ = "1.2.77"
+# this new verison sleeps for 30 seconds in case of any error/exception and kees retrying infinitely
 
 from time import sleep, time
 import stomp
@@ -9,34 +9,26 @@ import json
 import socket
 
 
-def connect_and_subscribe(conn, api_key, client_id, event=""):
-    retries = 1 if event in "start" else 10
-    # keep retryinh to re-connect
-    for retry in range(retries):
-        try:
-            print(f"{client_id} Attempting re/connection {retry} on", event)
-            acknowledgement_mode = "client-individual"
-            headers = {
-                "activemq.subcriptionName": client_id,
-                "activemq.subscriptionName": client_id,
-            }
-            conn.start()
-            conn.connect(api_key, api_key, wait=True, headers={"client-id": client_id})
-            conn.subscribe(
-                destination="/topic/bmrsTopic",
-                ack=acknowledgement_mode,
-                id=client_id,
-                headers=headers,
-            )
-        except stomp.exception.ConnectFailedException:
-            print('stomp.exception.ConnectFailedException')
-        except OSError:
-            print('OSError: [Errno 0] Error')
-        except Exception as e:
-            print(e)
-        sleep(6)
-    # send a notification to the developer
-    # twilio.send_sms('38591835912392931)
+def connect_and_subscribe(conn, api_key, client_id):
+    # first disconnect before trying to reconnect
+    # try:
+    #     conn.disconnect()
+    # except:
+    #     print('conn.disconnect()')
+    # attemp a connect
+    acknowledgement_mode = "client-individual"
+    headers = {
+        "activemq.subcriptionName": client_id,
+        "activemq.subscriptionName": client_id,
+    }
+    conn.start()
+    conn.connect(api_key, api_key, wait=True, headers={"client-id": client_id})
+    conn.subscribe(
+        destination="/topic/bmrsTopic",
+        ack=acknowledgement_mode,
+        id=client_id,
+        headers=headers,
+    )
 
 
 def get_hostname():
@@ -54,8 +46,9 @@ class MyListener(stomp.ConnectionListener):
         self.client_id = client_id
 
     def on_error(self, headers, message):
-        print(f'ERROR! : "{message}"')
-        connect_and_subscribe(self.conn, self.api_key, self.client_id, event="error")
+        print(f'on_error! : "{message}"')
+        sleep(30)
+        connect_and_subscribe(self.conn, self.api_key, self.client_id)
 
     def on_message(self, headers, message):
         message = xmltodict.parse(message)
@@ -70,41 +63,40 @@ class MyListener(stomp.ConnectionListener):
 
     def on_disconnected(self):
         print("disconnected")
-        connect_and_subscribe(
-            self.conn, self.api_key, self.client_id, event="disconnected"
-        )
+        sleep(30)
+        connect_and_subscribe(self.conn, self.api_key, self.client_id)
 
     def on_heartbeat_timeout(self):
-        print(
-            "Oh damn - the heartbeats have timed out.... Lets try re-connecting 30 times"
-        )
-        try:
-            print("Reconnecting: Attempt: ", n)
-            connect_and_subscribe(
-                self.conn, self.api_key, self.client_id, event="heartbeat timeout"
-            )
-        except stomp.exception.ConnectFailedException:
-            # Oh, still can't reconnect
-            print("Reconnect attempt failed")
-            time.sleep(1)
+        print("heartbeat_timeout")
+        sleep(30)
+        connect_and_subscribe(self.conn, self.api_key, self.client_id)
 
 
 def connect_to_api(api_key, listener, client_id="", port=61613):
     """ Connect to the BMRS API. This function will get the XML data and convert it to JSON instantaneously!
-
     `api_key`:str is your API key that can be got by following [Guide](https://www.elexon.co.uk/documents/training-guidance/bsc-guidance-notes/bmrs-api-and-data-push-user-guide-2/).
     `client_id`:str is your Client ID that can be got together with the APIkey from the above step.
     `listener`:func is your custom function that receives & handles messages returned from the API. See a sample of this in our sample file sample_client.py on github.
     """
-    # generate client id if not supplied
-    if not client_id:
-        client_id = get_hostname()
-    # connect to API using stomp
-    conn = stomp.Connection12(
-        host_and_ports=[("api.bmreports.com", port)], use_ssl=True
-    )
-    conn.set_listener("", MyListener(conn, listener, api_key, client_id))
-    connect_and_subscribe(conn, api_key, client_id, event="start")
-    # check for new messages after every x seconds
-    while conn.is_connected():
-        sleep(1)
+    while True:
+        try:
+            # generate client id if not supplied
+            if not client_id:
+                client_id = get_hostname()
+            print('client_id', client_id)
+            # connect to API using stomp
+            conn = stomp.Connection12(
+                host_and_ports=[("api.bmreports.com", port)], use_ssl=True
+            )
+            conn.set_listener("", MyListener(conn, listener, api_key, client_id))
+            connect_and_subscribe(conn, api_key, client_id)
+            # check for new messages after every x seconds
+            while conn.is_connected():
+                sleep(1)
+        except Exception as e:
+            print(e)
+            sleep(30)
+        # except stomp.exception.ConnectFailedException:
+        #     print('stomp.exception.ConnectFailedException')
+        # except OSError:
+        #     print('OSError: [Errno 0] Error')
